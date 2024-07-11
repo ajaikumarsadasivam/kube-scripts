2e249d015a51a20e8d5ffb30befd5ba6776e07a4
ajaikumarsadasivam <30375802+ajaikumarsadasivam@users.noreply.github.com>
2024-07-11 11:25:05 -0400

    Update apps-stop.sh
    optimizing based on no.of lines

--- apps-stop.sh
+++ apps-stop.sh
@@ -7,16 +7,13 @@ rep=0
 
 # switch to the cluster-context
 kubectl config use-context $context
-echo "Stopping services for env: $ns"
+echo "Starting services for env: $ns"
 
-# scale down deployment
-echo "Scaling down the deployment-apps to $rep"
+# scale down apps
+echo "Scaling down the Apps to $rep"
 dcount=$(kubectl get deploy -n $ns | tail -n +2 | awk '{print $1}' | wc -l)
-kubectl scale deploy --replicas=$rep -n $ns
-echo "$dcount - Deployment-service(s) Stopped.!"
-
-# scale down statefulset
-echo "Scaling down the statefulset-apps to $rep"
 scount=$(kubectl get sts -n $ns | tail -n +2 | awk '{print $1}' | wc -l)
-kubectl scale sts --replicas=$rep -n $ns
+
+kubectl scale deploy,sts --all --replicas=$rep -n $ns
+echo "$dcount - Deployment-service(s) Stopped.!"
 echo "$scount - Statefulset-service(s) Stopped.!"
